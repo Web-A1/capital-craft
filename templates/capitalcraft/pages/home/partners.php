398afc9b50ca994494535508504bf3ef69c42b75
“A1_dev” <filipov.project@gmail.com>
2025-07-28 18:45:50 +0300

    дизайн по коду билдер

--- templates/capitalcraft/pages/home/partners.php
+++ templates/capitalcraft/pages/home/partners.php
@@ -1,15 +1,31 @@
-<section class="partners">
-    <div class="container">
-        <h2 class="partners__title">НАМ ДОВЕРЯЮТ <br>КРУПНЕЙШИЕ БАНКИ<br>И ЛИЗИНГОВЫЕ КОМПАНИИ</h2>
-        <div class="partners__viewport">
-            <ul class="partners__list">
-                <li class="partners__item"><img src="templates/capitalcraft/images/home/alfa.svg" alt="Alfa"></li>
-                <li class="partners__item"><img src="templates/capitalcraft/images/home/gpbal.svg" alt="GPB"></li>
-                <li class="partners__item"><img src="templates/capitalcraft/images/home/vtb.svg" alt="VTB"></li>
-                <li class="partners__item"><img src="templates/capitalcraft/images/home/alfa.svg" alt="Alfa"></li>
-                <li class="partners__item"><img src="templates/capitalcraft/images/home/gpbal.svg" alt="GPB"></li>
-                <li class="partners__item"><img src="templates/capitalcraft/images/home/vtb.svg" alt="VTB"></li>
-            </ul>
+<section class="partners-section">
+    <div class="partners-container">
+        <div class="partners-title">
+            <h2 class="title-text">НАМ ДОВЕРЯЮТ<br>КРУПНЕЙШИЕ БАНКИ<br>И ЛИЗИНГОВЫЕ КОМПАНИИ</h2>
+        </div>
+        <div class="partners-logos">
+            <div class="logos-carousel">
+                <div class="logos-track">
+                    <div class="logo-item">
+                        <img src="templates/capitalcraft/images/home/vtb.svg" alt="ВТБ" class="partner-logo">
+                    </div>
+                    <div class="logo-item">
+                        <img src="templates/capitalcraft/images/home/gpbal.svg" alt="Газпромбанк Автолизинг" class="partner-logo">
+                    </div>
+                    <div class="logo-item">
+                        <img src="templates/capitalcraft/images/home/alfa.svg" alt="Альфа Банк" class="partner-logo">
+                    </div>
+                    <div class="logo-item">
+                        <img src="templates/capitalcraft/images/home/vtb.svg" alt="ВТБ" class="partner-logo">
+                    </div>
+                    <div class="logo-item">
+                        <img src="templates/capitalcraft/images/home/gpbal.svg" alt="Газпромбанк Автолизинг" class="partner-logo">
+                    </div>
+                    <div class="logo-item">
+                        <img src="templates/capitalcraft/images/home/alfa.svg" alt="Альфа Банк" class="partner-logo">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
