feeee642891bfa14151fe084a518455649497a14
“A1_dev” <filipov.project@gmail.com>
2025-07-31 19:01:26 +0300

    моб меню - доработка кодекс

--- templates/capitalcraft/js/burger.js
+++ templates/capitalcraft/js/burger.js
@@ -4,10 +4,22 @@ const initBurger = () => {
   const burger = document.querySelector('.burger');
   const mobileNav = document.querySelector('.mobile-nav');
   if (!burger || !mobileNav) return;
+
+  const closeMenu = () => {
+    burger.classList.remove('active');
+    mobileNav.classList.remove('open');
+    document.body.classList.remove('menu-open');
+  };
+
   burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     mobileNav.classList.toggle('open');
     document.body.classList.toggle('menu-open');
+  });
+
+  const navLinks = mobileNav.querySelectorAll('a');
+  navLinks.forEach((link) => {
+    link.addEventListener('click', closeMenu);
   });
 };
 
