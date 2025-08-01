f6bf14a3cb309204541dd290050c6800a3074de4
“A1_dev” <filipov.project@gmail.com>
2025-08-01 12:53:28 +0300

    1

--- templates/capitalcraft/pages/home/partners.php
+++ templates/capitalcraft/pages/home/partners.php
@@ -11,9 +11,7 @@
             <div class="partners__carousel embla">
                 <div class="embla__viewport">
                     <div class="embla__container">
-                        <div class="embla__slide">
-                            <img src="/templates/capitalcraft/images/home/partners/vtb_color.svg" class="partner-logo" alt="ВТБ" />
-                        </div>
+
                         <div class="embla__slide">
                             <img src="/templates/capitalcraft/images/home/partners/gpbal_color.svg" class="partner-logo" alt="Газпромбанк Автолизинг" />
                         </div>
@@ -21,13 +19,16 @@
                             <img src="/templates/capitalcraft/images/home/partners/alfa_color.svg" class="partner-logo" alt="Альфа Бизнес" />
                         </div>
                         <div class="embla__slide">
-                            <img src="/templates/capitalcraft/images/home/partners/sber_color.svg" class="partner-logo" alt="Сбер Бизнес" />
+                            <img src="/templates/capitalcraft/images/home/partners/sovkom_color.svg" class="partner-logo" alt="Совкомбанк" />
                         </div>
                         <div class="embla__slide">
                             <img src="/templates/capitalcraft/images/home/partners/psb_color.svg" class="partner-logo" alt="Промсвязьбанк Бизнес" />
                         </div>
                         <div class="embla__slide">
-                            <img src="/templates/capitalcraft/images/home/partners/sovkom_color.svg" class="partner-logo" alt="Совкомбанк" />
+                            <img src="/templates/capitalcraft/images/home/partners/sber_color.svg" class="partner-logo" alt="Сбер Бизнес" />
+                        </div>
+                        <div class="embla__slide">
+                            <img src="/templates/capitalcraft/images/home/partners/vtb_color.svg" class="partner-logo" alt="ВТБ" />
                         </div>
                     </div>
                 </div>
