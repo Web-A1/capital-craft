3fd076fea2efb2a313e86ee6ed9561f7ef22cfab
“A1_dev” <filipov.project@gmail.com>
2025-08-05 07:29:45 +0300

    работает и скрол и свайп!

--- templates/capitalcraft/js/home/show_case_swiper.js
+++ templates/capitalcraft/js/home/show_case_swiper.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   new Swiper('.show-case__swiper', {
-    direction: 'horizontal',
-    mousewheel: false,
     slidesPerView: 1,
+    direction: 'horizontal',
+    mousewheel: { enabled: false }, // базовый вариант для мобильных
     pagination: {
       el: '.show-case__pagination',
       clickable: true,
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     breakpoints: {
       768: {
         direction: 'vertical',
-        mousewheel: true,
+        mousewheel: { enabled: true }, // включаем колесо и вертикальное направление
       },
     },
   });
