2cd8b277aafb9727d1fc13e05b809d33589d57cc
“A1_dev” <filipov.project@gmail.com>
2025-08-08 01:37:44 +0300

    dhjlt ,jktt vtytt yjhv dbpefk

--- templates/capitalcraft/js/pages/faq/faq.js
+++ templates/capitalcraft/js/pages/faq/faq.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const questions = document.querySelectorAll('.faq__question');
+  const questions = document.querySelectorAll('.faq__question');
 
-    questions.forEach(function (question) {
-        question.addEventListener('click', function () {
-            const isExpanded = question.getAttribute('aria-expanded') === 'true';
+  questions.forEach(function (question) {
+    question.addEventListener('click', function () {
+      const isExpanded = question.getAttribute('aria-expanded') === 'true';
 
-            questions.forEach(function (q) {
-                q.setAttribute('aria-expanded', 'false');
-            });
+      questions.forEach(function (q) {
+        q.setAttribute('aria-expanded', 'false');
+      });
 
-            question.setAttribute('aria-expanded', String(!isExpanded));
-        });
+      question.setAttribute('aria-expanded', String(!isExpanded));
     });
+  });
 });
