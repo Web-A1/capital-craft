6f5e682247c4dd785cfe3329651da54760c5e322
“A1_dev” <filipov.project@gmail.com>
2025-08-07 10:32:58 +0300

    создал корректно override для faq

---
+++ templates/capitalcraft/html/com_content/article/faq.php
@@ -0,0 +1,11 @@
+<?php defined('_JEXEC') or die; ?>
+<section class="faq">
+    <h1>Часто задаваемые вопросы</h1>
+
+    <div class="faq__item">
+        <h3>Вопрос?</h3>
+        <div class="faq__answer">Ответ...</div>
+    </div>
+
+    <!-- свой кастомный HTML -->
+</section>
