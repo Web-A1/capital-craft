b1c748fd4e61dabfa42616ee287dafb768845d29
“A1_dev” <filipov.project@gmail.com>
2025-08-08 01:03:11 +0300

    1

--- templates/capitalcraft/html/com_content/article/faq.php
+++ templates/capitalcraft/html/com_content/article/faq.php
@@ -7,13 +7,19 @@ require __DIR__ . '/../../../data/faq.php';
 
 <section class="faq">
     <div class="faq__container">
-        <div class="faq__accordion">
-            <?php foreach ($faq as $item): ?>
-                <div class="faq__item">
-                    <h3 class="faq__question"><?= htmlspecialchars($item['q'], ENT_QUOTES, 'UTF-8'); ?></h3>
-                    <div class="faq__answer"><?= htmlspecialchars($item['a'], ENT_QUOTES, 'UTF-8'); ?></div>
-                </div>
-            <?php endforeach; ?>
+        <div class="faq__content">
+            <div class="faq__title-block">
+                <div class="faq__subtitle">часто задаваемые вопросы</div>
+                <h1 class="faq__title">Сильные решения начинаются с неудобных вопросов</h1>
+            </div>
+            <div class="faq__accordion">
+                <?php foreach ($faq as $item): ?>
+                    <div class="faq__item">
+                        <h3 class="faq__question"><?= htmlspecialchars($item['q'], ENT_QUOTES, 'UTF-8'); ?></h3>
+                        <div class="faq__answer"><?= htmlspecialchars($item['a'], ENT_QUOTES, 'UTF-8'); ?></div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
         <figure class="faq__image">
             <img src="/templates/capitalcraft/images/faq/faq_hand.webp" alt="FAQ">
