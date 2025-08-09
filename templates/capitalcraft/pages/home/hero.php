756a84b91ee77c05a0578d7e50c2e26dff1f089b
“A1_dev” <filipov.project@gmail.com>
2025-08-09 10:51:49 +0300

    “1”

--- templates/capitalcraft/pages/home/hero.php
+++ templates/capitalcraft/pages/home/hero.php
@@ -4,7 +4,7 @@
             <h1 class="hero__title">Мы создаем <br> точные решения <br> для вашего капитала</h1>
             <p class="hero__text">CAPITAL CRAFT — бутиковое агентство инвестиционных решений, специализирующееся на привлечении финансирования для бизнеса. Мы помогаем компаниям найти оптимальные стратегии роста и максимально реализовать их потенциал</p>
             <a href="#cases" class="btn-main hero__btn">
-                <span>кейсы</span>
+                <span>завершенные кейсы</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 17 17 7m0 0H8m9 0v9" />
                 </svg>
