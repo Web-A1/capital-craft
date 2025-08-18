a8cfdb6698a449fa947ecec693828e23f33055ae
“A1_dev” <filipov.project@gmail.com>
2025-08-18 10:15:32 +0300

    refactor: implement CSS Grid with automatic content-based line sizing for philosophy block

--- templates/capitalcraft/pages/home/philosophy.php
+++ templates/capitalcraft/pages/home/philosophy.php
@@ -9,7 +9,14 @@
 
             <div class="philosophy__right" data-node-id="12991:131">
                 <div class="philosophy__grid">
-                    <article class="philosophy-item" data-node-id="12991:133">
+                    <!-- Grid areas for lines and content -->
+                    <div class="philosophy__line philosophy__line--top"></div>
+                    <div class="philosophy__line philosophy__line--right"></div>
+                    <div class="philosophy__line philosophy__line--bottom"></div>
+                    <div class="philosophy__line philosophy__line--left"></div>
+                    
+                    <!-- Content items -->
+                    <article class="philosophy-item philosophy-item--1" data-node-id="12991:133">
                         <div class="philosophy-item__icon" aria-hidden="true" data-node-id="12991:134">
                             <svg class="philosophy-item__svg" viewBox="0 0 57 57" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                 <path d="M39.8972 56L56 39.8972V17.1028L39.8972 1H17.1028L1 17.1028V39.8972L17.1028 56H39.8972Z" stroke="currentColor" stroke-miterlimit="10"/>
@@ -23,21 +30,21 @@
                         </div>
                     </article>
 
-                    <article class="philosophy-item" data-node-id="12991:144">
+                    <article class="philosophy-item philosophy-item--2" data-node-id="12991:144">
                         <div class="philosophy-item__icon" aria-hidden="true" data-node-id="12991:145">
                             <svg class="philosophy-item__svg" viewBox="0 0 58 68" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path d="M28.8747 17.4857L42.8299 25.7286L28.8747 33.9714L14.9194 25.7286L28.8747 17.4857Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M28.8747 33.9715L14.9194 25.7286V42.2143L28.8747 50.4572V33.9715Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M14.9195 25.7285L1 17.4856V33.9713L14.9195 42.2142V25.7285Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M28.8747 50.4571L14.9194 42.2143V58.7L28.8747 66.9428V50.4571Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M14.9195 42.2142L1 33.9714V50.4571L14.9195 58.6999V42.2142Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M14.9195 9.24287L28.8747 17.4857L14.9195 25.7286L1 17.4857L14.9195 9.24287Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M42.83 9.24287L56.7494 17.4857L42.83 25.7286L28.8747 17.4857L42.83 9.24287Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M28.8747 1L42.8299 9.24285L28.8747 17.4857L14.9194 9.24285L28.8747 1Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M28.8747 33.9715L42.83 25.7286V42.2143L28.8747 50.4572V33.9715Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M42.83 25.7286L56.7495 17.4857V33.9714L42.83 42.2143V25.7286Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M28.8747 50.4571L42.83 42.2143V58.7L28.8747 66.9428V50.4571Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M42.83 42.2142L56.7495 33.9714V50.4571L42.83 58.6999V42.2142Z" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M28.8747 17.4857L42.8299 25.7286L28.8747 33.9714L14.9194 25.7286L28.8747 17.4857З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M28.8747 33.9715L14.9194 25.7286V42.2143L28.8747 50.4572V33.9715З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M14.9195 25.7285L1 17.4856V33.9713L14.9195 42.2142V25.7285З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M28.8747 50.4571L14.9194 42.2143V58.7L28.8747 66.9428V50.4571З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M14.9195 42.2142L1 33.9714V50.4571L14.9195 58.6999V42.2142З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M14.9195 9.24287L28.8747 17.4857L14.9195 25.7286L1 17.4857L14.9195 9.24287З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M42.83 9.24287L56.7494 17.4857L42.83 25.7286L28.8747 17.4857L42.83 9.24287З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M28.8747 1L42.8299 9.24285L28.8747 17.4857L14.9194 9.24285L28.8747 1З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M28.8747 33.9715L42.83 25.7286V42.2143L28.8747 50.4572V33.9715З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M42.83 25.7286L56.7495 17.4857V33.9714L42.83 42.2143V25.7286З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M28.8747 50.4571L42.83 42.2143V58.7L28.8747 66.9428V50.4571З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M42.83 42.2142L56.7495 33.9714V50.4571L42.83 58.6999V42.2142З" stroke="currentColor" stroke-miterlimit="10"/>
                             </svg>
                         </div>
                         <div class="philosophy-item__content">
@@ -46,13 +53,13 @@
                         </div>
                     </article>
 
-                    <article class="philosophy-item" data-node-id="12991:170">
+                    <article class="philosophy-item philosophy-item--3" data-node-id="12991:170">
                         <div class="philosophy-item__icon" aria-hidden="true" data-node-id="12991:171">
                             <svg class="philosophy-item__svg" viewBox="0 0 57 58" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path d="M35.3457 15.0531V1H49.0839V15.0531C49.0839 18.9255 52.1674 22.0796 55.953 22.0796V36.1327C44.5656 36.1327 35.3457 26.6703 35.3457 15.0531Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M14.7383 22.0796H1V8.02654H14.7383C18.5239 8.02654 21.6074 4.87241 21.6074 1H35.3456C35.3456 12.6172 26.1257 22.0796 14.7383 22.0796Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M21.6074 43.1592V57.2123H7.86913V43.1592C7.86913 39.2868 4.78565 36.1327 1 36.1327V22.0796C12.3875 22.0796 21.6074 31.5108 21.6074 43.1592Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M42.2147 36.1328H55.953V50.1858H42.2147C38.4291 50.1858 35.3456 53.34 35.3456 57.2124H21.6074C21.6074 45.5639 30.8273 36.1328 42.2147 36.1328Z" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M35.3457 15.0531V1H49.0839V15.0531C49.0839 18.9255 52.1674 22.0796 55.953 22.0796В36.1327C44.5656 36.1327 35.3457 26.6703 35.3457 15.0531З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M14.7383 22.0796H1V8.02654H14.7383C18.5239 8.02654 21.6074 4.87241 21.6074 1H35.3456C35.3456 12.6172 26.1257 22.0796 14.7383 22.0796З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M21.6074 43.1592V57.2123H7.86913В43.1592C7.86913 39.2868 4.78565 36.1327 1 36.1327В22.0796C12.3875 22.0796 21.6074 31.5108 21.6074 43.1592З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M42.2147 36.1328H55.953В50.1858H42.2147C38.4291 50.1858 35.3456 53.34 35.3456 57.2124H21.6074C21.6074 45.5639 30.8273 36.1328 42.2147 36.1328З" stroke="currentColor" stroke-miterlimit="10"/>
                             </svg>
                         </div>
                         <div class="philosophy-item__content">
@@ -61,13 +68,13 @@
                         </div>
                     </article>
 
-                    <article class="philosophy-item" data-node-id="12991:182">
+                    <article class="philosophy-item philosophy-item--4" data-node-id="12991:182">
                         <div class="philosophy-item__icon" aria-hidden="true" data-node-id="12991:183">
                             <svg class="philosophy-item__svg" viewBox="0 0 58 57" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path d="M28.8747 56.3734C36.5721 56.3734 42.8121 50.1755 42.8121 42.53C42.8121 34.8846 36.5721 28.6867 28.8747 28.6867C21.1773 28.6867 14.9374 34.8846 14.9374 42.53C14.9374 50.1755 21.1773 56.3734 28.8747 56.3734Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M28.8747 28.6867C36.5721 28.6867 42.8121 22.4888 42.8121 14.8433C42.8121 7.19787 36.5721 1 28.8747 1C21.1773 1 14.9374 7.19787 14.9374 14.8433C14.9374 22.4888 21.1773 28.6867 28.8747 28.6867Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M42.8121 42.53C50.5095 42.53 56.7494 36.3321 56.7494 28.6866C56.7494 21.0412 50.5095 14.8433 42.8121 14.8433C35.1147 14.8433 28.8747 21.0412 28.8747 28.6866C28.8747 36.3321 35.1147 42.53 42.8121 42.53Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M14.9374 42.53C22.6348 42.53 28.8747 36.3321 28.8747 28.6866C28.8747 21.0412 22.6348 14.8433 14.9374 14.8433C7.23997 14.8433 1 21.0412 1 28.6866C1 36.3321 7.23997 42.53 14.9374 42.53Z" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M28.8747 56.3734C36.5721 56.3734 42.8121 50.1755 42.8121 42.53C42.8121 34.8846 36.5721 28.6867 28.8747 28.6867C21.1773 28.6867 14.9374 34.8846 14.9374 42.53C14.9374 50.1755 21.1773 56.3734 28.8747 56.3734З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M28.8747 28.6867C36.5721 28.6867 42.8121 22.4888 42.8121 14.8433C42.8121 7.19787 36.5721 1 28.8747 1C21.1773 1 14.9374 7.19787 14.9374 14.8433C14.9374 22.4888 21.1773 28.6867 28.8747 28.6867З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M42.8121 42.53C50.5095 42.53 56.7494 36.3321 56.7494 28.6866C56.7494 21.0412 50.5095 14.8433 42.8121 14.8433C35.1147 14.8433 28.8747 21.0412 28.8747 28.6866C28.8747 36.3321 35.1147 42.53 42.8121 42.53З" stroke="currentColor" stroke-miterlimit="10"/>
+                                <path d="M14.9374 42.53C22.6348 42.53 28.8747 36.3321 28.8747 28.6866C28.8747 21.0412 22.6348 14.8433 14.9374 14.8433C7.23997 14.8433 1 21.0412 1 28.6866C1 36.3321 7.23997 42.53 14.9374 42.53З" stroke="currentColor" stroke-miterlimit="10"/>
                             </svg>
                         </div>
                         <div class="philosophy-item__content">
