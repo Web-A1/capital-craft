33fd8f5c695b93886e4bcb5f03ad1b5c776ac283
“A1_dev” <filipov.project@gmail.com>
2025-08-18 10:25:51 +0300

    fix: replace inline SVG with image files and remove meaningless data-node-id attributes

--- templates/capitalcraft/pages/home/philosophy.php
+++ templates/capitalcraft/pages/home/philosophy.php
@@ -1,13 +1,13 @@
 <section class="philosophy frame section-with-divider" id="philosophy">
     <div class="container philosophy__container">
         <div class="philosophy__wrapper">
-            <div class="philosophy__left" data-node-id="12991:129">
-                <div class="philosophy__eyebrow" data-node-id="12991:130">философия capital craft</div>
+            <div class="philosophy__left">
+                <div class="philosophy__eyebrow">философия capital craft</div>
                 <h2 class="philosophy__title">Результат - это последовательность выверенных шагов</h2>
                 <p class="philosophy__desc">Мы не сопровождаем — мы ведём. Погружаемся в задачу, считаем риски, разрабатываем стратегию и доводим до результата</p>
             </div>
 
-            <div class="philosophy__right" data-node-id="12991:131">
+            <div class="philosophy__right">
                 <div class="philosophy__grid">
                     <!-- Grid areas for lines and content -->
                     <div class="philosophy__line philosophy__line--top"></div>
@@ -16,13 +16,9 @@
                     <div class="philosophy__line philosophy__line--left"></div>
                     
                     <!-- Content items -->
-                    <article class="philosophy-item philosophy-item--1" data-node-id="12991:133">
-                        <div class="philosophy-item__icon" aria-hidden="true" data-node-id="12991:134">
-                            <svg class="philosophy-item__svg" viewBox="0 0 57 57" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path d="M39.8972 56L56 39.8972V17.1028L39.8972 1H17.1028L1 17.1028V39.8972L17.1028 56H39.8972Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M36.1083 46.8333L46.8333 36.1083V20.9222L36.1083 10.1666H20.8916L10.1666 20.9222V36.1083L20.8916 46.8333H36.1083Z" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M32.2889 37.6667L37.6667 32.2889V24.7112L32.2889 19.3334H24.7111L19.3333 24.7112V32.2889L24.7111 37.6667H32.2889Z" stroke="currentColor" stroke-miterlimit="10"/>
-                            </svg>
+                    <article class="philosophy-item philosophy-item--1">
+                        <div class="philosophy-item__icon" aria-hidden="true">
+                            <img src="images/home/ph_item1.svg" alt="Уверенность" class="philosophy-item__svg">
                         </div>
                         <div class="philosophy-item__content">
                             <h3 class="philosophy-item__title">уверенность</h3>
@@ -30,22 +26,9 @@
                         </div>
                     </article>
 
-                    <article class="philosophy-item philosophy-item--2" data-node-id="12991:144">
-                        <div class="philosophy-item__icon" aria-hidden="true" data-node-id="12991:145">
-                            <svg class="philosophy-item__svg" viewBox="0 0 58 68" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path d="M28.8747 17.4857L42.8299 25.7286L28.8747 33.9714L14.9194 25.7286L28.8747 17.4857З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M28.8747 33.9715L14.9194 25.7286V42.2143L28.8747 50.4572V33.9715З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M14.9195 25.7285L1 17.4856V33.9713L14.9195 42.2142V25.7285З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M28.8747 50.4571L14.9194 42.2143V58.7L28.8747 66.9428V50.4571З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M14.9195 42.2142L1 33.9714V50.4571L14.9195 58.6999V42.2142З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M14.9195 9.24287L28.8747 17.4857L14.9195 25.7286L1 17.4857L14.9195 9.24287З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M42.83 9.24287L56.7494 17.4857L42.83 25.7286L28.8747 17.4857L42.83 9.24287З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M28.8747 1L42.8299 9.24285L28.8747 17.4857L14.9194 9.24285L28.8747 1З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M28.8747 33.9715L42.83 25.7286V42.2143L28.8747 50.4572V33.9715З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M42.83 25.7286L56.7495 17.4857V33.9714L42.83 42.2143V25.7286З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M28.8747 50.4571L42.83 42.2143V58.7L28.8747 66.9428V50.4571З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M42.83 42.2142L56.7495 33.9714V50.4571L42.83 58.6999V42.2142З" stroke="currentColor" stroke-miterlimit="10"/>
-                            </svg>
+                    <article class="philosophy-item philosophy-item--2">
+                        <div class="philosophy-item__icon" aria-hidden="true">
+                            <img src="images/home/ph_item2.svg" alt="Ответственность" class="philosophy-item__svg">
                         </div>
                         <div class="philosophy-item__content">
                             <h3 class="philosophy-item__title">ОТВЕТСТВЕННОСТЬ</h3>
@@ -53,14 +36,9 @@
                         </div>
                     </article>
 
-                    <article class="philosophy-item philosophy-item--3" data-node-id="12991:170">
-                        <div class="philosophy-item__icon" aria-hidden="true" data-node-id="12991:171">
-                            <svg class="philosophy-item__svg" viewBox="0 0 57 58" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path d="M35.3457 15.0531V1H49.0839V15.0531C49.0839 18.9255 52.1674 22.0796 55.953 22.0796В36.1327C44.5656 36.1327 35.3457 26.6703 35.3457 15.0531З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M14.7383 22.0796H1V8.02654H14.7383C18.5239 8.02654 21.6074 4.87241 21.6074 1H35.3456C35.3456 12.6172 26.1257 22.0796 14.7383 22.0796З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M21.6074 43.1592V57.2123H7.86913В43.1592C7.86913 39.2868 4.78565 36.1327 1 36.1327В22.0796C12.3875 22.0796 21.6074 31.5108 21.6074 43.1592З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M42.2147 36.1328H55.953В50.1858H42.2147C38.4291 50.1858 35.3456 53.34 35.3456 57.2124H21.6074C21.6074 45.5639 30.8273 36.1328 42.2147 36.1328З" stroke="currentColor" stroke-miterlimit="10"/>
-                            </svg>
+                    <article class="philosophy-item philosophy-item--3">
+                        <div class="philosophy-item__icon" aria-hidden="true">
+                            <img src="images/home/ph_item3.svg" alt="Доверие" class="philosophy-item__svg">
                         </div>
                         <div class="philosophy-item__content">
                             <h3 class="philosophy-item__title">доверие</h3>
@@ -68,14 +46,9 @@
                         </div>
                     </article>
 
-                    <article class="philosophy-item philosophy-item--4" data-node-id="12991:182">
-                        <div class="philosophy-item__icon" aria-hidden="true" data-node-id="12991:183">
-                            <svg class="philosophy-item__svg" viewBox="0 0 58 57" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path d="M28.8747 56.3734C36.5721 56.3734 42.8121 50.1755 42.8121 42.53C42.8121 34.8846 36.5721 28.6867 28.8747 28.6867C21.1773 28.6867 14.9374 34.8846 14.9374 42.53C14.9374 50.1755 21.1773 56.3734 28.8747 56.3734З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M28.8747 28.6867C36.5721 28.6867 42.8121 22.4888 42.8121 14.8433C42.8121 7.19787 36.5721 1 28.8747 1C21.1773 1 14.9374 7.19787 14.9374 14.8433C14.9374 22.4888 21.1773 28.6867 28.8747 28.6867З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M42.8121 42.53C50.5095 42.53 56.7494 36.3321 56.7494 28.6866C56.7494 21.0412 50.5095 14.8433 42.8121 14.8433C35.1147 14.8433 28.8747 21.0412 28.8747 28.6866C28.8747 36.3321 35.1147 42.53 42.8121 42.53З" stroke="currentColor" stroke-miterlimit="10"/>
-                                <path d="M14.9374 42.53C22.6348 42.53 28.8747 36.3321 28.8747 28.6866C28.8747 21.0412 22.6348 14.8433 14.9374 14.8433C7.23997 14.8433 1 21.0412 1 28.6866C1 36.3321 7.23997 42.53 14.9374 42.53З" stroke="currentColor" stroke-miterlimit="10"/>
-                            </svg>
+                    <article class="philosophy-item philosophy-item--4">
+                        <div class="philosophy-item__icon" aria-hidden="true">
+                            <img src="images/home/ph_item4.svg" alt="Стратегия" class="philosophy-item__svg">
                         </div>
                         <div class="philosophy-item__content">
                             <h3 class="philosophy-item__title">стратегия</h3>
