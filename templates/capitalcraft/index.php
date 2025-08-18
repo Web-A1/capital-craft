5a6fe818782bda0a14a12803f2c9231be50c1be2
“A1_dev” <filipov.project@gmail.com>
2025-08-18 13:21:34 +0300

    feat: add products block with responsive design and SVG icons

--- templates/capitalcraft/index.php
+++ templates/capitalcraft/index.php
@@ -27,7 +27,7 @@ if ($isFaq) {
   <!-- Шрифты -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Golos+Text:wght@400;500;700&family=Onest:wght@400;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Golos+Text:wght@400;500;700&family=Onest:wght@400;700&family=Syne:wght@400;500;600&display=swap" rel="stylesheet">
 
   <!-- Критические стили -->
   <link rel="stylesheet" href="templates/capitalcraft/css/critical.css">
