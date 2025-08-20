7ed3715e447bd444971112bbf7142e027b1aa44d
“A1_dev” <filipov.project@gmail.com>
2025-08-20 11:49:25 +0300

    PHP update 2025-08-20 11:49:25

--- templates/capitalcraft/pages/home/philosophy.php
+++ templates/capitalcraft/pages/home/philosophy.php
@@ -2,7 +2,7 @@
     <div class="container philosophy__container">
         <div class="philosophy__wrapper">
             <div class="philosophy__left">
-                <div class="philosophy__eyebrow">философия capital craft</div>
+                <div class="philosophy__subtitle">философия capital craft</div>
                 <h2 class="philosophy__title">Результат - это последовательность выверенных шагов</h2>
                 <p class="philosophy__desc">Мы не сопровождаем — мы ведём. Погружаемся в задачу, считаем риски, разрабатываем стратегию и доводим до результата</p>
             </div>
