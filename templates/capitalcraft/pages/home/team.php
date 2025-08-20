ea320cb330f6f0139bf3b8ef2ab9da3914f52e28
“A1_dev” <filipov.project@gmail.com>
2025-08-20 11:07:52 +0300

    PHP update 2025-08-20 11:07:51

--- templates/capitalcraft/pages/home/team.php
+++ templates/capitalcraft/pages/home/team.php
@@ -29,23 +29,27 @@
                 </div>
             </div>
             <div class="team__right">
-                <div class="team__image team__image--main">
-                    <img src="/templates/capitalcraft/images/home/image_team_1.webp" alt="Team member 1" loading="lazy">
-                    <div class="team__image-overlay">
-                        <a href="#" class="team__view-button">
-                            <span>view</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 9 12">
-                                <path d="M8.14878 7.14623C8.34291 6.95185 8.65835 6.95117 8.85332 7.14472C9.03057 7.32068 9.04725 7.59658 8.90296 7.79133L8.85484 7.84714L4.85336 11.8538C4.67635 12.031 4.39862 12.0471 4.20328 11.9021L4.14733 11.8538L0.145188 7.84717C-0.0489644 7.6528 -0.0483113 7.33832 0.146647 7.14475C0.323882 6.96878 0.600686 6.95332 0.795419 7.098L0.851195 7.1462L4.50053 10.7992L8.14878 7.14623Z" fill="currentColor"/>
-                                <path d="M4.5 0C4.75313 0 4.96233 0.188103 4.99544 0.432153L5 0.5V10.5C5 10.7761 4.77614 11 4.5 11C4.24687 11 4.03767 10.8119 4.00456 10.5678L4 10.5V0.5C4 0.223858 4.22386 0 4.5 0Z" fill="currentColor"/>
-                            </svg>
-                        </a>
+                <div class="team__gallery">
+                    <div class="team__image team__image--main">
+                        <img src="/templates/capitalcraft/images/home/image_team_1.webp" alt="Фотография команды Capital Craft - основной снимок" loading="lazy">
+                        <div class="team__image-overlay">
+                            <a href="#" class="team__view-button" aria-label="Посмотреть детали">
+                                <span>view</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 9 12">
+                                    <path d="M8.14878 7.14623C8.34291 6.95185 8.65835 6.95117 8.85332 7.14472C9.03057 7.32068 9.04725 7.59658 8.90296 7.79133L8.85484 7.84714L4.85336 11.8538C4.67635 12.031 4.39862 12.0471 4.20328 11.9021L4.14733 11.8538L0.145188 7.84717C-0.0489644 7.6528 -0.0483113 7.33832 0.146647 7.14475C0.323882 6.96878 0.600686 6.95332 0.795419 7.098L0.851195 7.1462L4.50053 10.7992L8.14878 7.14623Z" fill="currentColor"/>
+                                    <path d="M4.5 0C4.75313 0 4.96233 0.188103 4.99544 0.432153L5 0.5V10.5C5 10.7761 4.77614 11 4.5 11C4.24687 11 4.03767 10.8119 4.00456 10.5678L4 10.5V0.5C4 0.223858 4.22386 0 4.5 0Z" fill="currentColor"/>
+                                </svg>
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="team__image team__image--secondary">
-                    <img src="/templates/capitalcraft/images/home/image_team_2.webp" alt="Team member 2" loading="lazy">
-                </div>
-                <div class="team__image team__image--secondary">
-                    <img src="/templates/capitalcraft/images/home/image_team_3.webp" alt="Team member 3" loading="lazy">
+                    <div class="team__images-secondary">
+                        <div class="team__image team__image--secondary">
+                            <img src="/templates/capitalcraft/images/home/image_team_2.webp" alt="Фотография команды Capital Craft - дополнительный снимок" loading="lazy">
+                        </div>
+                        <div class="team__image team__image--secondary">
+                            <img src="/templates/capitalcraft/images/home/image_team_3.webp" alt="Фотография команды Capital Craft - дополнительный снимок" loading="lazy">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
