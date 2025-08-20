10c5bd4f9f0db471f98c5f322eefecb3fb7dfd05
“A1_dev” <filipov.project@gmail.com>
2025-08-20 10:52:54 +0300

    PHP update 2025-08-20 10:52:54

--- templates/capitalcraft/pages/home/team.php
+++ templates/capitalcraft/pages/home/team.php
@@ -1,8 +1,52 @@
 <section class="team frame section-with-divider" id="team">
-    <div class="container">
-        <img src="/templates/capitalcraft/images/_team.webp"
-            alt="Команда Capital - Craft"
-            loading="lazy"
-            style="display:block; width:100%; height:auto;">
+    <div class="container team__container">
+        <div class="team__wrapper">
+            <div class="team__left">
+                <div class="team__title-block">
+                    <div class="team__subtitle">
+                        <p>команда</p>
+                    </div>
+                    <div class="team__title">
+                        <p>Эксперты, на которых можно положиться</p>
+                    </div>
+                </div>
+                <div class="team__description">
+                    <p>Каждый из нас строил бизнес, принимал риски и ответственность, проходил через найм, обязательства перед людьми и непростые решения. Мы не читаем про бизнес — мы его ведём. На своих деньгах, со своими людьми, под свою ответственность.</p>
+                    <p>Команда Capital Craft — это палитра практического опыта: лизинг, строительство, международная торговля, финансовый консалтинг. Всё, чем мы занимаемся, — прожито изнутри, пройдено в деле и проверено временем.</p>
+                    <p>Сегодня мы вместе не потому, что «так сложилось», а потому что в этой команде каждый усиливает друг друга и вместе мы можем больше!</p>
+                </div>
+                <div class="team__button-wrapper">
+                    <a href="#" class="btn-main team__button">
+                        <span>подробнее</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 16">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6.63672 0C7.0087 0 7.31658 0.273914 7.36523 0.628906L7.37207 0.727539V13.5117L12.0176 8.89355C12.3039 8.60893 12.7691 8.60823 13.0566 8.8916C13.3181 9.14925 13.3427 9.55271 13.1299 9.83789L13.0596 9.91992L7.1582 15.7861C7.09079 15.8531 7.01225 15.9029 6.92969 15.9385C6.9173 15.9438 6.90433 15.9475 6.8916 15.9521C6.85904 15.9641 6.82663 15.9752 6.79297 15.9824C6.78298 15.9846 6.77283 15.9856 6.7627 15.9873C6.72573 15.9936 6.68878 15.9973 6.65137 15.998C6.64652 15.9981 6.64159 16 6.63672 16C6.62819 16 6.61979 15.9974 6.61133 15.9971C6.58252 15.9961 6.55395 15.9946 6.52539 15.9902C6.50685 15.9874 6.48876 15.9836 6.4707 15.9795C6.44697 15.974 6.42358 15.9678 6.40039 15.96C6.3803 15.9532 6.36106 15.9449 6.3418 15.9365C6.32365 15.9286 6.30567 15.9206 6.28809 15.9111C6.26605 15.8993 6.24521 15.886 6.22461 15.8721C6.21656 15.8666 6.20709 15.8632 6.19922 15.8574L6.17188 15.834C6.16055 15.8248 6.14941 15.8155 6.13867 15.8057L6.11621 15.7871L0.213867 9.91992C-0.0723551 9.63542 -0.0713593 9.17507 0.21582 8.8916C0.477213 8.63393 0.885651 8.61139 1.17285 8.82324L1.25488 8.89355L5.90234 13.5107V0.727539C5.90234 0.325904 6.23098 6.17158e-05 6.63672 0Z" fill="currentColor"/>
+                        </svg>
+                    </a>
+                </div>
+                <div class="team__icon">
+                    <img src="/templates/capitalcraft/images/home/icon-team-home.svg" alt="Team Icon" loading="lazy">
+                </div>
+            </div>
+            <div class="team__right">
+                <div class="team__image team__image--main">
+                    <img src="/templates/capitalcraft/images/home/image_team_1.webp" alt="Team member 1" loading="lazy">
+                    <div class="team__image-overlay">
+                        <a href="#" class="team__view-button">
+                            <span>view</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 9 12">
+                                <path d="M8.14878 7.14623C8.34291 6.95185 8.65835 6.95117 8.85332 7.14472C9.03057 7.32068 9.04725 7.59658 8.90296 7.79133L8.85484 7.84714L4.85336 11.8538C4.67635 12.031 4.39862 12.0471 4.20328 11.9021L4.14733 11.8538L0.145188 7.84717C-0.0489644 7.6528 -0.0483113 7.33832 0.146647 7.14475C0.323882 6.96878 0.600686 6.95332 0.795419 7.098L0.851195 7.1462L4.50053 10.7992L8.14878 7.14623Z" fill="currentColor"/>
+                                <path d="M4.5 0C4.75313 0 4.96233 0.188103 4.99544 0.432153L5 0.5V10.5C5 10.7761 4.77614 11 4.5 11C4.24687 11 4.03767 10.8119 4.00456 10.5678L4 10.5V0.5C4 0.223858 4.22386 0 4.5 0Z" fill="currentColor"/>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+                <div class="team__image team__image--secondary">
+                    <img src="/templates/capitalcraft/images/home/image_team_2.webp" alt="Team member 2" loading="lazy">
+                </div>
+                <div class="team__image team__image--secondary">
+                    <img src="/templates/capitalcraft/images/home/image_team_3.webp" alt="Team member 3" loading="lazy">
+                </div>
+            </div>
+        </div>
     </div>
 </section>
