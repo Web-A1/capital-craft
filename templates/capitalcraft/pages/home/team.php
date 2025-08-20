677b6ef71df1a5f1651582874cb063f95818f78e
“A1_dev” <filipov.project@gmail.com>
2025-08-20 10:46:56 +0300

    PHP update 2025-08-20 10:46:56

--- templates/capitalcraft/pages/home/team.php
+++ templates/capitalcraft/pages/home/team.php
@@ -1,51 +1,8 @@
 <section class="team frame section-with-divider" id="team">
-    <div class="container team__container">
-        <div class="team__wrapper">
-            <div class="team__left">
-                <div class="team__eyebrow">наша команда</div>
-                <h2 class="team__title">Профессионалы, которые доводят проекты до результата</h2>
-                <p class="team__desc">Наша команда объединяет экспертов с многолетним опытом в финансировании, аудите и стратегическом планировании. Мы работаем как единый механизм, где каждый специалист вносит свой вклад в общий успех проекта</p>
-                <div class="team__icon">
-                    <img src="/templates/capitalcraft/images/home/icon-team-home.svg" alt="Team Icon" loading="lazy">
-                </div>
-            </div>
-
-            <div class="team__right">
-                <div class="team__members">
-                    <article class="team-member team-member--1">
-                        <div class="team-member__photo">
-                            <img src="/templates/capitalcraft/images/home/image_team_1.webp" alt="Член команды 1" loading="lazy">
-                        </div>
-                        <div class="team-member__content">
-                            <h3 class="team-member__name">Александр Петров</h3>
-                            <p class="team-member__position">Руководитель проектов</p>
-                            <p class="team-member__description">Специалист по структурированному финансированию с опытом работы в крупнейших банках России</p>
-                        </div>
-                    </article>
-
-                    <article class="team-member team-member--2">
-                        <div class="team-member__photo">
-                            <img src="/templates/capitalcraft/images/home/image_team_2.webp" alt="Член команды 2" loading="lazy">
-                        </div>
-                        <div class="team-member__content">
-                            <h3 class="team-member__name">Мария Сидорова</h3>
-                            <p class="team-member__position">Финансовый аналитик</p>
-                            <p class="team-member__description">Эксперт по финансовому моделированию и оценке инвестиционных проектов</p>
-                        </div>
-                    </article>
-
-                    <article class="team-member team-member--3">
-                        <div class="team-member__photo">
-                            <img src="/templates/capitalcraft/images/home/image_team_3.webp" alt="Член команды 3" loading="lazy">
-                        </div>
-                        <div class="team-member__content">
-                            <h3 class="team-member__name">Дмитрий Козлов</h3>
-                            <p class="team-member__position">Юридический консультант</p>
-                            <p class="team-member__description">Специалист по корпоративному праву и структурированию сделок</p>
-                        </div>
-                    </article>
-                </div>
-            </div>
-        </div>
+    <div class="container">
+        <img src="/templates/capitalcraft/images/_team.webp"
+            alt="Команда Capital - Craft"
+            loading="lazy"
+            style="display:block; width:100%; height:auto;">
     </div>
 </section>
