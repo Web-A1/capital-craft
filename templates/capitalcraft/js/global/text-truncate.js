42528d6fe7d148d3da999d9050a9bc33e49daf38
“A1_dev” <filipov.project@gmail.com>
2025-08-21 10:11:04 +0300

    JS rebuild 2025-08-21 10:11:04

--- templates/capitalcraft/js/global/text-truncate.js
+++ templates/capitalcraft/js/global/text-truncate.js
@@ -108,19 +108,12 @@ function resetTextTruncate() {
   const descriptions = document.querySelectorAll('.products__item-description');
   
   descriptions.forEach(description => {
-    // Убираем классы с параграфа и восстанавливаем оригинальный текст
+    // Убираем классы с параграфа
     const textElement = description.querySelector('p');
     if (textElement) {
       textElement.classList.remove('truncated', 'expanded');
       textElement.style.cursor = '';
       textElement.removeAttribute('title');
-      
-      // Восстанавливаем оригинальный текст
-      const originalText = textElement.getAttribute('data-original-text');
-      if (originalText) {
-        textElement.textContent = originalText;
-        textElement.removeAttribute('data-original-text');
-      }
     }
   });
 }
