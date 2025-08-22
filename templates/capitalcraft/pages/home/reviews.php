192b59612fbd2957a2eb4f0121d1b8f3980a7716
“A1_dev” <filipov.project@gmail.com>
2025-08-22 14:06:01 +0300

    PHP update 2025-08-22 14:06:00

--- templates/capitalcraft/pages/home/reviews.php
+++ templates/capitalcraft/pages/home/reviews.php
@@ -1,7 +1,7 @@
 <section class="reviews frame section-with-divider">
     <div class="container">
         <img src="/templates/capitalcraft/images/_reviews.webp"
-            alt="Продукты Capital - Craft"
+            alt="Отзывы Capital - Craft"
             loading="lazy"
             style="display:block; width:100%; height:auto;">
     </div>
