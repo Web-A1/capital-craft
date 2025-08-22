09e85de3a1b947ca2787c28dae2a10dfc337026f
“A1_dev” <filipov.project@gmail.com>
2025-08-22 12:31:02 +0300

    PHP update 2025-08-22 12:31:02

--- templates/capitalcraft/index.php
+++ templates/capitalcraft/index.php
@@ -99,6 +99,13 @@ if ($isFaq) {
     <!-- Hreflang для языковой версии -->
     <link rel="alternate" hreflang="ru-RU" href="https://capital-craft.ru/">
     <link rel="alternate" hreflang="x-default" href="https://capital-craft.ru/">
+    
+    <!-- Специальные мета-теги для FAQ страницы -->
+    <?php if ($isFaq): ?>
+      <meta name="keywords" content="инвестиции, финансирование, капитал, бизнес, лизинг, банки, привлечение средств">
+      <meta name="author" content="Capital Craft">
+      <meta name="copyright" content="© 2025 Capital Craft. Все права защищены.">
+    <?php endif; ?>
   <?php endif; ?>
 
   <!-- Шрифты -->
