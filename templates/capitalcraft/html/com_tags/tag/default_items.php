0efaa722a17e86e73f952973472ed0d4eeeca8a0
“A1_dev” <filipov.project@gmail.com>
2025-09-10 12:13:57 +0300

    | 12:13 10/09

--- templates/capitalcraft/html/com_tags/tag/default_items.php
+++ templates/capitalcraft/html/com_tags/tag/default_items.php
@@ -1,5 +1,5 @@
 <?php
-defined('_JEXEC') or die;
+defined("_JEXEC") or die();
 
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
@@ -8,22 +8,31 @@ use Joomla\Component\Tags\Site\Helper\RouteHelper;
 
 /** @var \Joomla\Component\Tags\Site\View\Tag\HtmlView $this */
 
-if (empty($this->items)) : ?>
+if (empty($this->items)): ?>
   <div class="alert alert-info">
     <span class="icon-info-circle" aria-hidden="true"></span>
-    <span class="visually-hidden"><?php echo Text::_('INFO'); ?></span>
-    <?php echo Text::_('COM_TAGS_NO_ITEMS'); ?>
+    <span class="visually-hidden"><?php echo Text::_("INFO"); ?></span>
+    <?php echo Text::_("COM_TAGS_NO_ITEMS"); ?>
   </div>
-<?php else : ?>
+<?php else: ?>
   <div class="blog-list">
-    <?php foreach ($this->items as $i => $item) : ?>
+    <?php foreach ($this->items as $i => $item): ?>
       <?php
-        $images = json_decode($item->core_images ?: '{}');
-        $intro  = !empty($images->image_intro) ? $images->image_intro : null;
-        $alt    = !empty($images->image_intro_alt) ? $images->image_intro_alt : '';
-        $link   = Route::_(RouteHelper::getItemRoute($item->content_item_id, $item->core_alias, $item->core_catid, $item->core_language, $item->type_alias, $item->router));
-        $date   = $item->core_created_time ?? $item->core_publish_up ?? '';
-        $excerpt= HTMLHelper::_('string.truncate', strip_tags($item->core_body ?? ''), 200);
+      $images = json_decode($item->core_images ?: "{}");
+      $intro = !empty($images->image_intro) ? $images->image_intro : null;
+      $alt = !empty($images->image_intro_alt) ? $images->image_intro_alt : "";
+      $link = Route::_(
+          RouteHelper::getItemRoute(
+              $item->content_item_id,
+              $item->core_alias,
+              $item->core_catid,
+              $item->core_language,
+              $item->type_alias,
+              $item->router,
+          ),
+      );
+      $date = $item->core_created_time ?? ($item->core_publish_up ?? "");
+      $excerpt = HTMLHelper::_("string.truncate", strip_tags($item->core_body ?? ""), 250);
       ?>
       <article class="blog-card">
         <div class="blog-card__grid">
@@ -41,18 +50,22 @@ if (empty($this->items)) : ?>
             </div>
 
             <div class="blog-card__meta">
-              <?php if ($date) : ?>
-                <time class="blog-card__date" datetime="<?php echo HTMLHelper::_('date', $date, 'c'); ?>">
-                  <?php echo HTMLHelper::_('date', $date, Text::_('DATE_FORMAT_LC3')); ?>
+              <?php if ($date): ?>
+                <time class="blog-card__date" datetime="<?php echo HTMLHelper::_("date", $date, "c"); ?>">
+                  <?php echo HTMLHelper::_("date", $date, Text::_("DATE_FORMAT_LC3")); ?>
                 </time>
               <?php endif; ?>
             </div>
           </div>
 
-          <?php if ($intro) : ?>
+          <?php if ($intro): ?>
             <figure class="blog-card__image">
               <a href="<?php echo $link; ?>">
-                <img src="<?php echo htmlspecialchars($intro, ENT_QUOTES, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($alt, ENT_QUOTES, 'UTF-8'); ?>">
+                <img src="<?php echo htmlspecialchars(
+                    $intro,
+                    ENT_QUOTES,
+                    "UTF-8",
+                ); ?>" alt="<?php echo htmlspecialchars($alt, ENT_QUOTES, "UTF-8"); ?>">
               </a>
             </figure>
           <?php endif; ?>
@@ -61,4 +74,3 @@ if (empty($this->items)) : ?>
     <?php endforeach; ?>
   </div>
 <?php endif;
-
