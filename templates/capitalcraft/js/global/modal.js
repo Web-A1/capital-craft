c928a7154f7278b5c704c96d914f1a20a7494042
“A1_dev” <filipov.project@gmail.com>
2025-09-14 13:37:44 +0300

    | 13:37 14/09

--- templates/capitalcraft/js/global/modal.js
+++ templates/capitalcraft/js/global/modal.js
@@ -1,36 +1,42 @@
-'use strict';
+"use strict";
 
-import MicroModal from '../vendor/micromodal.es.min.js';
+import MicroModal from "../vendor/micromodal.es.min.js";
 
 export const initModal = () => {
   MicroModal.init({
     disableScroll: true,
     disableFocus: true,
     onShow: () => {
-      const form = document.getElementById('contactForm');
+      const form = document.getElementById("contactForm");
       if (form) {
-        form.style.display = 'flex';
+        form.style.display = "flex";
         form.reset();
-        const err = form.querySelector('.form-error');
-        if (err) err.style.display = 'none';
-        const consentErr = form.querySelector('.consent-error');
-        if (consentErr) consentErr.style.display = 'none';
-        const errorResult = form.querySelector('.form-result.error');
-        if (errorResult) errorResult.style.display = 'none';
+        const err = form.querySelector(".form-error");
+        if (err) err.style.display = "none";
+        const consentErr = form.querySelector(".consent-error");
+        if (consentErr) consentErr.style.display = "none";
+        const errorResult = form.querySelector(".form-result.error");
+        if (errorResult) errorResult.style.display = "none";
       }
-      const header = document.querySelector('#contact-modal .modal__header');
-      if (header) header.style.display = 'flex';
+      const header = document.querySelector("#contact-modal .modal__header");
+      if (header) header.style.display = "flex";
       const successBox = document.querySelector(
-        '#contact-modal .modal__success'
+        "#contact-modal .modal__success"
       );
-      if (successBox) successBox.style.display = 'none';
-      document.body.classList.add('modal-open');
+      if (successBox) successBox.style.display = "none";
+      const scrollBarWidth =
+        window.innerWidth - document.documentElement.clientWidth;
+      if (scrollBarWidth > 0) {
+        document.body.style.paddingRight = `${scrollBarWidth}px`;
+      }
+      document.body.classList.add("modal-open");
     },
     onClose: () => {
-      document.body.classList.remove('modal-open');
+      document.body.classList.remove("modal-open");
+      document.body.style.paddingRight = "";
       if (window.headerControl) {
         window.headerControl.pin();
       }
-    },
+    }
   });
 };
