ef8a4c2af07766ad57f226935c6718b4219f5d7c
“A1_dev” <filipov.project@gmail.com>
2025-09-18 12:42:39 +0300

    | 12:42 18/09

--- templates/capitalcraft/html/com_tags/tag/default.php
+++ templates/capitalcraft/html/com_tags/tag/default.php
@@ -1,7 +1,13 @@
 <?php
 defined("_JEXEC") or die();
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Router\Route;
+use Joomla\Component\Content\Site\Helper\RouteHelper as ContentRouteHelper;
+use Joomla\Component\Tags\Site\Helper\RouteHelper as TagsRouteHelper;
+
+require_once __DIR__ . "/../../../helpers/TagFilterHelper.php";
 
 /** @var \Joomla\Component\Tags\Site\View\Tag\HtmlView $this */
 $htag = $this->params->get("show_page_heading") ? "h2" : "h1";
@@ -17,6 +23,134 @@ $htag = $this->params->get("show_page_heading") ? "h2" : "h1";
       </<?php echo $htag; ?>>
     </header>
 
+    <?php
+    $app = Factory::getApplication();
+    $menu = $app->getMenu();
+    $blogMenu = $menu->getItems("alias", "blog", true);
+    $blogCategoryId = 0;
+
+    if ($blogMenu && isset($blogMenu->query["id"])) {
+        $blogCategoryId = (int) $blogMenu->query["id"];
+    }
+
+    $includeSub = false;
+    $maxLevels = 0;
+
+    if ($blogMenu && method_exists($blogMenu, "getParams")) {
+        $menuParams = $blogMenu->getParams();
+        if ($menuParams) {
+            $includeSub = (bool) $menuParams->get("show_subcategory_content", "0");
+            $maxLevels = $includeSub ? (int) $menuParams->get("show_subcategory_content", "1") : 0;
+        }
+    }
+
+    if ($blogCategoryId <= 0 && $blogMenu) {
+        $blogCategoryId = isset($blogMenu->query["catid"]) ? (int) $blogMenu->query["catid"] : 0;
+    }
+
+    $availableTags = [];
+
+    if ($blogCategoryId > 0) {
+        $availableTags = CapitalcraftTagFilterHelper::getBlogTags($blogCategoryId, [
+            "includeSubcategories" => $includeSub,
+            "maxSubLevels" => $maxLevels,
+        ]);
+    } else {
+        $availableTags = CapitalcraftTagFilterHelper::getAllTags();
+    }
+
+    $currentTagId = 0;
+    $currentTagAlias = "";
+
+    if (!empty($this->item) && \is_array($this->item)) {
+        $firstTag = $this->item[0] ?? null;
+        if ($firstTag) {
+            $currentTagId = (int) ($firstTag->id ?? 0);
+            $currentTagAlias = strtolower($firstTag->alias ?? "");
+        }
+    }
+
+    if (!$currentTagId) {
+        $rawId = $app->input->getString("id", "");
+        if ($rawId !== "") {
+            $parts = explode(":", $rawId, 2);
+            $currentTagId = (int) ($parts[0] ?? 0);
+            if (!empty($parts[1])) {
+                $currentTagAlias = strtolower($parts[1]);
+            }
+        }
+    }
+
+    if ($currentTagId && $currentTagAlias === "") {
+        $db = Factory::getDbo();
+        $user = Factory::getUser();
+        $levels = array_map("intval", $user->getAuthorisedViewLevels());
+        if (empty($levels)) {
+            $levels = [0];
+        }
+        $languageTag = $app->getLanguage()->getTag() ?: "*";
+        $languageCondition =
+            $languageTag === "*"
+                ? "1=1"
+                : $db->quoteName("t.language") . " IN (" . $db->quote("*") . "," . $db->quote($languageTag) . ")";
+        $tagQuery = $db
+            ->getQuery(true)
+            ->select($db->quoteName("alias"))
+            ->from($db->quoteName("#__tags", "t"))
+            ->where($db->quoteName("t.id") . " = " . (int) $currentTagId)
+            ->where($db->quoteName("t.published") . " = 1")
+            ->where("t.access IN (" . implode(",", $levels) . ")")
+            ->where($languageCondition)
+            ->setLimit(1);
+        $db->setQuery($tagQuery);
+        $aliasFromDb = (string) $db->loadResult();
+        if ($aliasFromDb !== "") {
+            $currentTagAlias = strtolower($aliasFromDb);
+        }
+    }
+    ?>
+
+    <?php if (!empty($availableTags)): ?>
+      <nav class="blog__tags-nav" aria-label="Навигация по тегам">
+        <ul class="blog-tags__cloud blog-tags__cloud--nowrap">
+          <?php
+          $blogRoute = null;
+          if ($blogCategoryId > 0) {
+              $blogRoute = Route::_(ContentRouteHelper::getCategoryRoute($blogCategoryId));
+          } elseif ($blogMenu) {
+              $blogRoute = Route::_("index.php?Itemid=" . (int) $blogMenu->id);
+          }
+          if ($blogRoute): ?>
+            <li class="blog-tags__tag">
+              <a
+                class="blog-tags__link<?php echo $currentTagAlias === "" ? " is-active" : ""; ?>"
+                href="<?php echo $blogRoute; ?>"
+                data-tag-alias=""
+                <?php echo $currentTagAlias === "" ? 'aria-current="page"' : ""; ?>
+              >Все статьи</a>
+            </li>
+          <?php endif;
+          ?>
+          <?php foreach ($availableTags as $tagOption): ?>
+            <?php
+            $tagAliasLower = strtolower($tagOption->alias ?? "");
+            $isActive = $currentTagAlias === $tagAliasLower;
+            $tagRoute = Route::_(TagsRouteHelper::getTagRoute((int) $tagOption->id . ":" . ($tagOption->alias ?? "")));
+            ?>
+            <li class="blog-tags__tag">
+              <a
+                class="blog-tags__link<?php echo $isActive ? " is-active" : ""; ?>"
+                href="<?php echo $tagRoute; ?>"
+                data-tag-alias="<?php echo htmlspecialchars($tagAliasLower, ENT_QUOTES, "UTF-8"); ?>"
+                <?php echo $isActive ? 'aria-current="page"' : ""; ?>
+              >#<?php echo htmlspecialchars($tagOption->title, ENT_QUOTES, "UTF-8"); ?></a>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </nav>
+      <div class="visually-hidden" aria-live="polite" aria-atomic="true" id="blog-filter-status"></div>
+    <?php endif; ?>
+
     <?php echo $this->loadTemplate("items"); ?>
 
     <?php if (
@@ -29,37 +163,26 @@ $htag = $this->params->get("show_page_heading") ? "h2" : "h1";
     <?php endif; ?>
 
     <?php
-    // Other tags block: show all published tags used by articles, except current
-    $db = Joomla\CMS\Factory::getDbo();
-    $idParam = Joomla\CMS\Factory::getApplication()->input->get("id", "", "STRING");
-    $currentTagId = (int) explode(":", (string) $idParam)[0];
-    $q = $db
-        ->getQuery(true)
-        ->select("t.id, t.title")
-        ->from($db->quoteName("#__tags", "t"))
-        ->join(
-            "INNER",
-            $db->quoteName("#__contentitem_tag_map", "m") .
-                " ON m.tag_id = t.id AND m.type_alias = " .
-                $db->quote("com_content.article"),
-        )
-        ->where("t.published = 1")
-        ->where("t.id != " . (int) $currentTagId)
-        ->group("t.id")
-        ->order("t.title ASC");
-    $db->setQuery($q);
-    $allTags = (array) $db->loadObjectList();
+    $otherTags = [];
+    if ($currentTagId) {
+        $otherTags = CapitalcraftTagFilterHelper::getAllTags(["excludeTagId" => $currentTagId]);
+    } else {
+        $otherTags = CapitalcraftTagFilterHelper::getAllTags();
+    }
     ?>
 
-    <?php if (!empty($allTags)): ?>
+    <?php if (!empty($otherTags)): ?>
       <section class="blog-tags__others">
         <div class="blog__subtitle">Другие теги</div>
         <ul class="blog-tags__cloud">
-          <?php foreach ($allTags as $tg): ?>
+          <?php foreach ($otherTags as $tg): ?>
+            <?php $tagRoute = Route::_(TagsRouteHelper::getTagRoute((int) $tg->id . ":" . ($tg->alias ?? ""))); ?>
             <li class="blog-tags__tag">
-              <a class="blog-tags__link" href="<?php echo JRoute::_(
-                  "index.php?option=com_tags&view=tag&id=" . (int) $tg->id,
-              ); ?>">#<?php echo htmlspecialchars($tg->title, ENT_QUOTES, "UTF-8"); ?></a>
+              <a class="blog-tags__link" href="<?php echo $tagRoute; ?>">#<?php echo htmlspecialchars(
+    $tg->title,
+    ENT_QUOTES,
+    "UTF-8",
+); ?></a>
             </li>
           <?php endforeach; ?>
         </ul>
