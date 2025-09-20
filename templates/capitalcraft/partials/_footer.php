17faf017763549a4a7d3da1dc2dff4388db04a48
“A1_dev” <filipov.project@gmail.com>
2025-09-20 15:12:01 +0300

    Откат к коммиту e27f048a (15:53 19.09) с сохранением откорректированного футера
    - Откат всех файлов к состоянию до 16:30 19.09.2025
    - Сохранены изменения в footer.php и footer.less (откорректированный футер)
    - Убрана сложная система мобильной навигации
    - Восстановлена простая логика работы меню

--- templates/capitalcraft/partials/_footer.php
+++ templates/capitalcraft/partials/_footer.php
@@ -1,4 +1,4 @@
-<footer class="footer frame frame--footer" id="contacts">
+<footer class="footer frame frame--footer">
     <div class="footer-container">
         <!-- Top Block -->
         <div class="footer-top-block">
@@ -37,7 +37,7 @@
                 </div>
 
                 <!-- Contacts -->
-                <div class="footer-contacts">
+                <div class="footer-contacts" id="contacts">
                     <div class="footer-section-title">
                         <span>Контакты</span>
                     </div>
@@ -55,7 +55,7 @@
                 <!-- Messengers -->
                 <div class="footer-messengers">
                     <div class="footer-section-title">
-                        <span>Месседжеры</span>
+                        <span>Мессенджеры</span>
                     </div>
 
                     <!-- WhatsApp -->
